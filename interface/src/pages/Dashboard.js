73e3893286f847c35d6f9f0ff1aa49361331b0d8
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-02 10:27:20 -0400

    reorg

--- interface/src/pages/Dashboard.js
+++ interface/src/pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default function Dashboard() {
     return (
         <div>
-            
+            <Navbar></Navbar>
         </div>
     )
 }
